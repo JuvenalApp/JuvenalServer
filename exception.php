4fa9382cf2f8c732236fb05542b94df6b99b440c
Andrew <ajbates@gmail.com>
2016-03-11 00:28:28 -0500

    Permissions and Scope, AKA useful API keys

--- exception.php
+++ exception.php
@@ -2,22 +2,22 @@
 
 class BadRequestException extends Exception { }
 
-class MySQLiNotConnectedError extends Exception { }
-
-class MySQLiStatementNotPreparedException extends Exception { }
-
 class InvalidJsonException extends Exception { }
-
-class MySQLiInsertQueryFailureException extends Exception { }
-
-class MySQLiRowNotInsertedException extends Exception { }
-
-class MySQLiSelectQueryFailedException extends Exception { }
-
-class MySQLiNothingSelectedException extends Exception { }
 
 class UnsanitizedInputException extends Exception {}
 
 class ApiKeyNotPrivilegedException extends Exception {}
+
+
+class MySQLiNotConnectedError extends Exception { }
+class MySQLiStatementNotPreparedException extends Exception { }
+
+class MySQLiSelectQueryFailedException extends Exception { }
+class MySQLiInsertQueryFailureException extends Exception { }
+class MySQLiUpdateQueryFailureException extends Exception { }
+class MySQLiDeleteQueryFailureException extends Exception { }
+
+class MySQLiRowNotInsertedException extends Exception { }
+class MySQLiNothingSelectedException extends Exception { }
 
 ?>
