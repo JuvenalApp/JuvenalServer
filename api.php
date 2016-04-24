31843f0f9f19589a20842af43b012e01c2016a89
Andrew <ajbates@gmail.com>
2016-04-24 17:59:23 -0400

    Finishing touches for now.

--- api.php
+++ api.php
@@ -1038,7 +1038,7 @@ EOF;
                             [ 'd' => $jsonRequest['latitude'] ],
                             [ 'd' => $jsonRequest['longitude'] ],
                             [ 's' => $jsonRequest['state'] ],
-                            [ 'i' => intval($dialbackNumber) ]
+                            [ 's' => $dialbackNumber ]
                         ]
                     );
 
